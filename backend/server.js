9486b876f325481f93b722ec4106ba94c51e864e
jules <jules.lemorvan01@gmail.com>
2025-06-23 18:11:57 +0200

    try deployement with render

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,27 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(cors());
 
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>  {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(5001, () => {
   connectDB();
